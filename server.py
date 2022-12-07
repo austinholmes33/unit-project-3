e3c47ede0bb35c91b769aadda24ad1de012383b3
Austin Holmes <austin.holmes4@gmail.com>
2022-12-07 14:10:29 -0700

    indentation error on server.py fixed

--- server.py
+++ server.py
@@ -67,8 +67,8 @@ def empty_cart():
     return redirect("/cart")
 
 @app.errorhandler(404)
-    def error_404(e):
-        return render_template("404.html")
+def error_404(e):
+    return render_template("404.html")
 
 if __name__ == "__main__":
     app.env = "development"
